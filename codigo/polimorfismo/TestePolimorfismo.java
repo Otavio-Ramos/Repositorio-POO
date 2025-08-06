9c866fbf5e922f867b46c79d844335b17f871ac9
Otavio Ramos <ramosotavio354@gmail.com>
2025-08-06 01:37:48 -0300

    Update TestePolimorfismo.java

--- codigo/polimorfismo/TestePolimorfismo.java
+++ codigo/polimorfismo/TestePolimorfismo.java
@@ -1,1 +1,11 @@
+public class TestePolimorfismo {
+    public static void main(String[] args) {
+        Criatura c1 = new CriaturaFogo();
+        Criatura c2 = new CriaturaAgua();
+
+        c1.atacar(); 
+        c2.atacar(); 
+        c1.atacar("Explosão flamejante"); 
+    }
+}
 
