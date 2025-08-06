1fe2516cb2f5d2e9b28dc5c270162d6e0a79dad7
Otavio Ramos <ramosotavio354@gmail.com>
2025-08-06 01:38:28 -0300

    Update CriaturaTerra.java

--- codigo/abstrata/CriaturaTerra.java
+++ codigo/abstrata/CriaturaTerra.java
@@ -1,1 +1,7 @@
+public class CriaturaTerra extends CriaturaElemental {
+    @Override
+    public void usarPoder() {
+        System.out.println("Criatura de terra cria uma muralha de pedra!");
+    }
+}
 
