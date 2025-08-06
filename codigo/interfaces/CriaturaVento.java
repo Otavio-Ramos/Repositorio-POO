a5b98d72e5549a5c375114aa14ee670c1c628655
Otavio Ramos <ramosotavio354@gmail.com>
2025-08-06 01:39:29 -0300

    Update CriaturaVento.java

--- codigo/interfaces/CriaturaVento.java
+++ codigo/interfaces/CriaturaVento.java
@@ -1,1 +1,10 @@
+public class CriaturaVento implements Curavel, Voador {
+    public void curar() {
+        System.out.println("Criatura de vento se regenera com a brisa.");
+    }
+
+    public void voar() {
+        System.out.println("Criatura de vento voa pelas correntes de ar.");
+    }
+}
 
