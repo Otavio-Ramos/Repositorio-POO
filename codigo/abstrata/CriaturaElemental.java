61fac23b18744eea8cdd25c70e61b4f70b4e8cc1
Otavio Ramos <ramosotavio354@gmail.com>
2025-08-06 01:38:13 -0300

    Update CriaturaElemental.java

--- codigo/abstrata/CriaturaElemental.java
+++ codigo/abstrata/CriaturaElemental.java
@@ -1,1 +1,8 @@
+public abstract class CriaturaElemental {
+    public abstract void usarPoder();
+
+    public void descrever() {
+        System.out.println("Sou uma criatura elemental.");
+    }
+}
 
