1aff1ee3645254a0f1c13adbf6f257c2a624fb59
Otavio Ramos <ramosotavio354@gmail.com>
2025-08-06 01:36:42 -0300

    Update Criatura.java

--- codigo/polimorfismo/Criatura.java
+++ codigo/polimorfismo/Criatura.java
@@ -1,1 +1,10 @@
+public class Criatura {
+    public void atacar() {
+        System.out.println("A criatura ataca!");
+    }
+
+    public void atacar(String poder) {
+        System.out.println("A criatura usa o poder: " + poder);
+    }
+}
 
